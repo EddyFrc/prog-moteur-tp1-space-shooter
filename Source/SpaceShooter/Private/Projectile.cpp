015e568c0133b62396ccec52c3ff5c31558cf90b
EddyFrc <eddyfrancou@gmail.com>
2025-09-18 12:31:18 -0600

    Base classe projectile

---
+++ Source/SpaceShooter/Private/Projectile.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Move(GetActorRotation().Vector());
+}
+
+// Called to bind functionality to input
+void AProjectile::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+// Sets default values
+AProjectile::AProjectile()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
---
+++ Source/SpaceShooter/Public/Projectile.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VolumePawn.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class SPACESHOOTER_API AProjectile : public AVolumePawn
+{
+	GENERATED_BODY()
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Sets default values for this pawn's properties
+	AProjectile();
+};
